c1f03f843f5c40104b7a070a7858cc11df131326
Apprentice <apprentice@devbootcamp.com>
2013-06-14 15:21:21 -0700

    User and survey controller added and the views and partials

---
+++ app/controllers/survey_controller.rb
@@ -0,0 +1,27 @@
+get '/surveys/:id' do
+
+  erb :showsurvey
+end
+
+post '/surveys/:id/completed' do
+
+  redirect '/'
+end
+
+get '/surveys/:id/new' do
+
+  erb :surveynew
+end
+
+post '/surveys/:id' do
+
+end
+
+get '/surveys/:id/creator/:creator_id' do
+  @creator = User.find(params[:creator_id])
+  redirect '/' unless @creator == current_user
+  erb :showresults
+end
+
+
+
